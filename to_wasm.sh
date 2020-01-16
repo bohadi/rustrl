d8f26726c1c69eebb6961dd5e01974730d43af5d
bohadi <bohadi@protonmail.com>
2020-01-16 15:45:35 +0800

    wasm

---
+++ to_wasm.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/sh
+
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen target/wasm32-unknown-unknown/release/rustrl.wasm --out-dir . --no-modules --no-typescript
+
+python -m http.server
